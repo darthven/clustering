943e15b3cb16a6259c2e13cba7d11cddb6f3845c
kokh0716 <konstantin.kharlambov@netcracker.com>
2017-10-26 14:53:45 +0300

    Input from console was fixed

--- main.jl
+++ main.jl
@@ -9,11 +9,11 @@ dataset = datasetFromCSV("creditcard.csv")
 
 # Getting values for dataset by column names
 println("Enter 1st column name, please")
-col1 = chomp(readline())
+col1 = readline()
 println("Enter 2nd column name, please")
-col2 = chomp(readline())
+col2 = readline()
 println("Enter 3rd column name, please")
-col3 = chomp(readline())
+col3 = readline()
 values = datasetValues(dataset, col1, col2, col3)
 
 # Uncomment if need hardcoded column names
@@ -21,7 +21,7 @@ values = datasetValues(dataset, col1, col2, col3)
 
 # Defining count of the clusters
 println("Enter clusters count, please")
-clustersCount = parse(Int64, chomp(readline()))
+clustersCount = parse(Int64, readline())
 
 # Uncomment if need hardcoded clusters' count
 # clustersCount = 6
